596ab2cf136591e39758c7d01f75be2d3dc0f9ae
Deepansh Dubey <56029566+deepanshdubey@users.noreply.github.com>
2022-11-18 20:24:12 +0530

    Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
+import React from 'react';
 import Navbar from './components/Navbar.js'
 import TextForm from './components/TextForm'
 import About from './components/About';
-
+import { useState } from 'react';
 
 function App() {
+  let [mode, setMode] = useState('light');
+
+  const toggleMode = ()=> {
+    console.log("Mode Toggled");
+
+    if(mode === 'light')
+    {
+      setMode('dark');
+    }
+    else{
+      setMode('light');
+    }
+  }
   return (
    <>
-   <Navbar title="Text Inspecto"/>
+   <Navbar title= "Text Inspecto"  mode={mode} /> 
    <TextForm/>
    <About/>
    </>
   );
 }
-
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,24 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import PropTypes from "prop-types";
 function myNavbar(props) {
+
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg={props.mode}  expand="lg">
       <Container>
-        <Navbar.Brand className='logo' href="#home"><img src={require("../TextUtil Logo.png")} alt="logo" /></Navbar.Brand>
+        <Navbar.Brand className="logo" href="#home">
+          <img src={require("../TextUtil Logo.png")} alt="logo" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">{props.title}</Nav.Link>
             <Nav.Link href="#link">About Us</Nav.Link>
+            <Form>
+              <Form.Check type="switch" onClick={props.toggleMode} id="custom-switch" label="Dark Mode" />
+            </Form>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -26,5 +34,5 @@ export default myNavbar;
 // }
 
 Navbar.defaultProps = {
-    title: "Set title here",
+  title: "Set title here",
 };
